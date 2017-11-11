705745283e233ff8b586726938777211609e0c34
Zack Shapiro <zack@zackshapiro.com>
2017-11-11 15:27:41 -0500

    Initial Commit

---
+++ Rai/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rai
+//
+//  Created by Zack Shapiro on 11/11/17.
+//  Copyright © 2017 Zack Shapiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
